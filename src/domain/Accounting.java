bdc2a85822c8430e92fa2de6378080a79a045929
OliverAndreasen <oliv2058@stud.kea.dk>
2021-11-23 12:45:55 +0100

    Members with debt and calculate subscription fee added

--- src/domain/Accounting.java
+++ src/domain/Accounting.java
@@ -6,17 +6,51 @@ public class Accounting {
     private int subscriptionTotal;
     private ArrayList<Member> membersWithDebt = new ArrayList<>();
 
-
-    public int calculateSubscriptionFee() {
-
-        return -1;
+    //@Author Oliver
+    public int calculateSubscriptionFee(Member member) {
+        int result;
+        if(member.getActiveStatus()){
+            if(member.getAge() <= 18) {
+                if(member.getAge() <= 60) {
+                    result = 1200;
+                }
+                else {
+                    result = 1600;
+                }
+            }
+            else {
+                result = 1000;
+            }
+        }
+        else {
+            result = 500;
+        }
+        return result;
     }
 
     public ArrayList getMembersWithDebt() {
         return membersWithDebt;
     }
 
-    public int projectedSubscriptionTotal(){
+    // @Author Oliver
+    public String getMembersWithDebtToString(){
+        StringBuilder result = new StringBuilder();
+        if(!membersWithDebt.isEmpty()){
+            for (Member member : membersWithDebt) {
+                result.append("Navn: ");
+                result.append(member.getName());
+                result.append("\nResistance: ");
+                result.append(calculateSubscriptionFee(member));
+                result.append(" kr.\n");
+            }
+        }
+        else {
+            result = new StringBuilder("Ingen medlemmer med resistance");
+        }
+        return result.toString();
+    }
+
+    public int projectedSubscriptionTotal() {
 
         return -1;
     }
@@ -25,7 +59,7 @@ public class Accounting {
         membersWithDebt.add(member);
     }
 
-    public void removeMemberWithDebt(Member member){
+    public void removeMemberWithDebt(Member member) {
         membersWithDebt.remove(member);
     }
- }
+}
