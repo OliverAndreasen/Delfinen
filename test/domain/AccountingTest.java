d3c0bb1949b87769056237228b57ed8c1fe029d3
alexander6273 <alex6273@stud.kea.dk>
2021-12-07 12:42:46 +0100

    Junit test subscription total

--- test/domain/AccountingTest.java
+++ test/domain/AccountingTest.java
@@ -1,6 +1,11 @@
 package domain;
 
+import database.Database;
+import database.FileHandler;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,10 +14,24 @@ class AccountingTest {
     @Test
     public void testCalculateSubscription(){
         //Arrange
+        Database db = new Database();
+        Accounting accounting = new Accounting();
+        ArrayList<Member> members = db.getAllMembers();
+
+        try {
+            db.loadMemberIdsWithDebt();
+        } catch (IOException e){
+
+        }
 
         //Act
+        for (Member member : members){
+            accounting.addSubscriptionTotal(accounting.calculateSubscriptionFee(member.getActiveStatus(),member.getAge()));
+        }
+
+        Integer expected = 31100;
 
         //Assert
+        assertEquals(expected, accounting.getSubscriptionTotal());
     }
-
 }
