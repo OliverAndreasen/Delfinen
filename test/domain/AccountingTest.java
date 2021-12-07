2c5557aea352d5c54da93ea7789c99e25171ebd7
alexander6273 <alex6273@stud.kea.dk>
2021-12-07 12:30:38 +0100

    Optimering

--- test/domain/AccountingTest.java
+++ test/domain/AccountingTest.java
@@ -1,7 +1,18 @@
 package domain;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountingTest {
+
+    @Test
+    public void testCalculateSubscription(){
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
 
 }
